7d64fdd5d406bcdd9be2f584a032c9a546b4c32b
Maxim <hristivi8383@gmail.com>
2021-05-04 21:23:05 +0300

    'alerts'

---
+++ scripts/script.js
@@ -0,0 +1,30 @@
+const name = document.querySelector('#name');
+const email = document.querySelector('#email');
+const message = document.querySelector('#message');
+const submit = document.querySelector('.submit');
+let isItEmpty = true;
+
+const createAlert = (text, color) => {
+    const alert = document.createElement('div')
+    alert.innerHTML = text;
+    alert.setAttribute('class', `alert ${color}`)
+    setTimeout(() => {
+        $('.alert').animate({
+            opacity: '0'
+        }, 500)
+    }, 2000)
+    document.querySelector('.navbar').appendChild(alert);
+    $('.alert').animate({
+        top: '100px'
+    }, 330)
+}
+
+
+submit.addEventListener('click', e => {
+    e.preventDefault();
+    if(name.value.replaceAll(' ', '') === '' || email.value.replaceAll(' ', '') === '' || message.value.replaceAll(' ', '') === '' ){ 
+        createAlert('Попълни Всичко', 'red')
+    } else {
+        createAlert('Съобщението е изпратено', 'green');
+    }
+})
